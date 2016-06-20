46ade4149589c656f88f094f2da129a71fbe1fe9
sdavidk <davidkohno500@gmail.com>
2016-06-20 08:05:20 -0400

    initial commit

---
+++ 04-Summer School/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Summer_School
+{
+    class Program
+    {
+        static string[] Students = new string[15];
+
+        static void Main(string[] args)
+
+        {        //1. Enroll a student
+                 //2. Unenroll a student
+                 //3. Print out the list of enrolled students
+                 //4. Exit
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+            while (true)
+            {
+
+                if (choice == 1)
+                {
+                    Enrollment();
+                }
+                else if (choice == 2)
+                {
+
+                }
+                else if (choice == 3)
+                {
+                    
+                }
+                else if (choice == 4_)
+               {
+
+                }
+            
+        }    
+
+        private static void Enrollment()
+        {
+            string student;
+            student = Console.ReadLine();
+            for (int i = 0, i < 15; i++)
+            {
+                if (Students[i] == null)
+                {
+                    Students[i] = student;
+                    break;
+                }
+            }
+           
+            
+
+
+        }
+    }
+}
